e3cb0a86f0b88c80c84058cd5206628a5931209c
Ngatmuri <142901620+Ngatmuri@users.noreply.github.com>
2023-11-19 15:28:07 +0700

    Update react.js

--- react.js
+++ react.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 
 const test = async () => {
     const browser = await puppeteer.launch({
-        headless:false,
+        headless:true,
         args: ['--no-sandbox'],
         userDataDir: './cache', 
         defaultViewport: {
